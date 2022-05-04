a8b47f0cf0b23e59ba40b4e8c5363bc725586bff
Ghostarijit <arijitbiswas484@gmail.com>
2022-05-04 22:33:34 +0530

    hi, I Am Arijit

--- src/controller/collegeController.js
+++ src/controller/collegeController.js
@@ -36,32 +36,6 @@ const createCollege = async(req, res) => {
 
 
 
-        // title validation
-        //  if (!title) return res.status(400).send({ status: false, msg: "title must be present" });
-        // if (typeof title !== "string") return res.status(400).send({ status: false, msg: "title should be string" });
-        // if (!(["Mr", "Mrs", "Miss"].includes(data.title))) return res.status(400).send({ status: false, msg: "plz write valid title" })
-
-        // email validation
-        // if (!email) {
-        //     return res.status(400).send({ status: false, msg: "email must be present" });
-        // }
-
-        // let regx = /^([a-zA-Z0-9\._]+)@([a-zA-Z])+\.([a-z]+)(.[a-z])?$/
-        //     // rege=/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
-        // let x = regx.test(email)
-        // if (!x) {
-        //      return res.status(400).send({ status: false, msg: "write the correct format for email" })
-        //  }
-        // let mail = await authorModel.findOne({ email: email.toLowerCase() })
-
-        // if (mail) return res.status(400).send({ status: false, msg: "this email is already present" })
-        // data.email = data.email.toLowerCase()
-
-        // password validation
-        //  if (!password) return res.status(400).send({ status: false, msg: "plz write the password" });
-        //  if (typeof password !== "string" || password.trim().length === 0) return res.status(400).send({ status: false, msg: "enter valid password" });
-        //  data.password = data.password.trim()
-
         let college = await collegeModel.create(data)
 
         res.status(201).send({ status: true, data: college })
